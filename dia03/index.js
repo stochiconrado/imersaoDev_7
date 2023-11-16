ac9140aebfd64fcb9d520fec17c90557138f5e32
Conrado Alberto da Silva Stochi <stochiconrado@gmail.com>
2023-11-16 09:10:37 -0300

    Dia 03 finalizado, sem edição

---
+++ dia03/index.js
@@ -0,0 +1,11 @@
+let listaFilmes = [
+  "https://upload.wikimedia.org/wikipedia/pt/thumb/1/1b/Schoolrockposter.jpg/210px-Schoolrockposter.jpg",
+  "https://1.bp.blogspot.com/-ImZPRqLsluE/WFK156_6pNI/AAAAAAAAYBY/0lEhNRF5wfQdLfr6hpT57_Jt2eBrE9H5wCLcB/s1600/arrival-kartoun-desert.jpg",
+  "https://musicart.xboxlive.com/7/94fc5000-0000-0000-0000-000000000002/504/image.jpg",
+  "https://upload.wikimedia.org/wikipedia/pt/7/76/10_Things_I_Hate_About_You.jpg",
+  "https://br.web.img2.acsta.net/medias/nmedia/18/91/08/82/20128877.JPG"
+];
+
+for (let index = 0; index < listaFilmes.length; index++) {
+  document.write(`<img src= ${listaFilmes[index]}>`);
+}
